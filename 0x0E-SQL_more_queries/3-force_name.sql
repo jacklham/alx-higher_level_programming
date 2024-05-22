4f9b13313cb14d070b76ed4556d0b65b425a3cc9
jacklham <jackkuelmazierlham@gmail.com>
2024-05-22 08:24:07 -0700

    adding

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,6 @@
 -- Lists all the tables of a database
 -- Query to list all tables of a database
-SHOW TABLES;
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
